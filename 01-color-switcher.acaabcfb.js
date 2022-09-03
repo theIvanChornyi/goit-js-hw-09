a0997dfaae5bb85dea2fe8cce053a30aeebb0d2a
theIvanChornyi <107806721+theIvanChornyi@users.noreply.github.com>
2022-09-03 20:02:55 +0000

    Deploying to gh-pages from @ theIvanChornyi/goit-js-hw-09@95f1a111b31fd63bad4cedd15be8e116b87a3ae7 🚀

--- 01-color-switcher.acaabcfb.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");let r=null;e.addEventListener("click",(()=>{e.disabled=!0,r=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),o.addEventListener("click",(()=>{clearInterval(r),e.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.acaabcfb.js.map
