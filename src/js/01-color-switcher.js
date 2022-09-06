5e0f7364bec921a735876b84dd6608e1235127e1
Ivan Chornyi <chorniyivan@icloud.com>
2022-09-06 17:30:19 +0300

    fixed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,8 +5,7 @@ let colorTimer = null;
 
 
 startBtn.addEventListener('click', () => {
-  startBtn.disabled = true;
-  stopBtn.disabled = false;
+  blockBtns(stopBtn, startBtn);
   pageBody.style.backgroundColor = getRandomHexColor();
   colorTimer = setInterval(() => {
     pageBody.style.backgroundColor = getRandomHexColor();
@@ -15,11 +14,15 @@ startBtn.addEventListener('click', () => {
   
 stopBtn.addEventListener('click', () => {
   clearInterval(colorTimer);
-  startBtn.disabled = false;
-  stopBtn.disabled = true;
+  blockBtns(startBtn, stopBtn);
 });
 
  
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function blockBtns(undisabledBtn, disabledBtn) {
+  disabledBtn.disabled = true;
+  undisabledBtn.disabled = false;
 }
